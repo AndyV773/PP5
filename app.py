0b8614faa1f37283e1f2afa3f08732ada69f8554
AndyV773 <ajv103@hotmail.com>
2025-05-06 14:31:05 +0100

    create multipage app for streamlit

--- app.py
+++ app.py
@@ -1,3 +1,15 @@
 import streamlit as st
+from app_pages.multipage import MultiPage
 
-st.write('hello world!')
+
+# load pages scripts
+from app_pages.page_summary import page_summary_body
+from app_pages.page_data_study import page_data_study_body
+
+app = MultiPage(app_name = "Stock Data Analysis") # Create an instance of the app 
+
+# Add your app pages here using .add_page()
+app.add_page("Quick Project Summary", page_summary_body)
+app.add_page("Data Study", page_data_study_body)
+
+app.run() # Run the app
---
+++ app_pages/page_data_study.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+
+def page_data_study_body():
+
+
+    st.write("### Data Study")
+    st.info(
+        f"* The client is interested in identifying patterns in historical stock data "
+        f"to uncover the most relevant variables that influence whether a stock's value "
+        f"will go up or down the next day."
+    )
+
+    
